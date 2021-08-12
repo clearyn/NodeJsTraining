f1e0dd3de4036f5c0dd48b13409fccbef85c1214
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-08-12 08:22:29 +0700

    12/8 - 1

--- NodeJS_Core_Function/app.js
+++ NodeJS_Core_Function/app.js
@@ -1,12 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
     //console.log(req.url, req.method, req.headers);
     const url = req.url;
+    const method = req.method;
+
     if(url === '/'){
         res.write('<html>');
         res.write('<head>Enter Some Message</head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button action="submit">Send</button></form></body>')
         res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
