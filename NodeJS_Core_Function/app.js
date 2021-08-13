698e41c9f01d4663074756ff3c664d9f521440e5
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-08-13 11:14:35 +0700

    8/13-1

--- NodeJS_Core_Function/app.js
+++ NodeJS_Core_Function/app.js
@@ -1,40 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-    //console.log(req.url, req.method, req.headers);
-    const url = req.url;
-    const method = req.method;
-
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head>Enter Some Message</head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button action="submit">Send</button></form></body>')
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head>My First Page</head>');
-    res.write('<body>Wow awesome</body>')
-    res.write('</html>');
-    res.end();
-});
-
+const routes = require('./routes');
+const server = http.createServer(routes.anoterHandler);
+console.log(routes.someText);
 server.listen(3000);
