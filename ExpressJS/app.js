902f3fbf547623074157c7147a9cbc5693c1ca24
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-08-16 07:26:45 +0700

    separate route using express

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(express.urlencoded({extended: false}));
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text", name="title"></input><button action="submit">send</button></form>')
-});
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use('/', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>hello from express</h1>');
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
---
+++ ExpressJS/routes/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text", name="title"></input><button action="submit">send</button></form>')
+});
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
