dc63943c4f781894c727caeb9912f4ce370658f3
Ryan Cleary <49626962+clearyn@users.noreply.github.com>
2021-08-17 10:25:57 +0700

    implement base directory

--- ExpressJS/routes/shop.js
+++ ExpressJS/routes/shop.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>hello from express</h1>');
+    //res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
